3431ccdf801ff409edddb45724e81c1a2397a982
vaswin <srinivas.enfs@gmail.com>
2015-02-26 18:34:33 +0530

    Added maven plugin.

--- Camel/src/main/groovy/CamelTestJetty.groovy
+++ Camel/src/main/groovy/CamelTestJetty.groovy
@@ -1,10 +1,10 @@
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.impl.DefaultCamelContext
 
-@Grab(group="org.apache.camel", module="camel-core", version="2.13.1")
+/*@Grab(group="org.apache.camel", module="camel-core", version="2.13.1")
 @Grab(group="org.apache.camel", module="camel-groovy", version="2.13.1")
 @Grab(group="org.apache.camel", module="camel-jetty", version="2.13.1")
-@Grab(group="org.slf4j", module="slf4j-jdk14", version="1.7.5")
+@Grab(group="org.slf4j", module="slf4j-jdk14", version="1.7.5")*/
 
 class MyRouteBuilder extends RouteBuilder{
 	int num =0;
